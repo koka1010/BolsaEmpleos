8d72aaa2c84fe5556d0d1290b96ce9fe5204346c
simon10 <simonmazo3@gmail.com>
2024-11-26 12:38:56 -0500

    servicios y presentacion

---
+++ asp_presentacion/Startup.cs
@@ -0,0 +1,49 @@
+﻿using lib_comunicaciones.Implementaciones;
+using lib_comunicaciones.Interfaces;
+using lib_presentaciones.Implementaciones;
+using lib_presentaciones.Interfaces;
+
+namespace asp_presentacion
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration? Configuration { set; get; }
+
+        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        {
+            // Comunicaciones
+            services.AddScoped<IProductosComunicacion, ProductosComunicacion>();
+            services.AddScoped<ITiposComunicacion, TiposComunicacion>();
+            // Presentaciones
+            services.AddScoped<IProductosPresentacion, ProductosPresentacion>();
+            services.AddScoped<ITiposPresentacion, TiposPresentacion>();
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddRazorPages();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.UseSession();
+            app.Run();
+        }
+    }
+}
