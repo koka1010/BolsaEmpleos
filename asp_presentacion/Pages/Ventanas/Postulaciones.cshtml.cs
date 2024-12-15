d88e2257be928a447d392917489eacb4c6ff447c
simon10 <simonmazo3@gmail.com>
2024-12-14 22:25:08 -0500

    ventanas

---
+++ asp_presentacion/Pages/Ventanas/Postulaciones.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_presentacion.Pages.Ventanas
+{
+    public class PostulacionesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ asp_presentacion/Pages/Ventanas/Vacantes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_presentacion.Pages.Ventanas
+{
+    public class VacantesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
